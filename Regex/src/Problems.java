93001cd544a3c22b8c7a39b1de93b78d460e7326
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2024-03-21 19:24:41 +0200

    Initial and first 6 problems

---
+++ Regex/src/Problems.java
@@ -0,0 +1,174 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Problems {
+	
+	public String problem1(BufferedReader reader) throws IOException
+	{
+		String line;
+		String res="";
+		res+="1\n";
+		Pattern pt = Pattern.compile("[a-zA-Z\\d._%+-]+@[a-zA-Z]+\\.[a-zA-Z]{2,}");
+		while(!(line = reader.readLine()).equals("end") )
+		{
+			System.out.println(line);
+			Matcher matcher = pt.matcher(line);
+			if(matcher.matches())
+			{
+				res += "valid email\n";
+			}
+			else
+			{
+				res += "invalid email\n";
+			}
+				
+		}
+		res += "x\n";
+		return res;
+				
+	}
+	
+	public String problem2(BufferedReader reader) throws IOException
+	{
+		String line;
+		String res="";
+		res+="2\n";
+		Pattern pt = Pattern.compile("\\(?\\d{3}\\)?[-.\\s]?\\d{3}[-.\\s]?\\d{4}");
+		while(!(line = reader.readLine()).equals("end") )
+		{
+			System.out.println(line);
+			Matcher matcher = pt.matcher(line);
+			if(matcher.matches())
+			{
+				res += "valid phone number\n";
+			}
+			else
+			{
+				res += "invalid phone number\n";
+			}
+				
+		}
+		res += "x\n";
+		return res;
+	}
+	
+	public String problem3(BufferedReader reader) throws IOException
+	{
+		String line;
+		String res="";
+		res+="3\n";
+		Pattern pt = Pattern.compile("\\d{1,4}[-\\/]\\d{1,2}[-\\/]\\d{1,4}");
+		while(!(line = reader.readLine()).equals("end") )
+		{
+			System.out.println(line);
+			Matcher matcher = pt.matcher(line);
+			if(matcher.matches())
+			{
+				res += "valid date\n";
+			}
+			else
+			{
+				res += "invalid date\n";
+			}
+				
+		}
+		res += "x\n";
+		return res;
+	}
+	
+	public String problem4(BufferedReader reader) throws IOException
+	{
+		String line;
+		String res="";
+		res+="4\n";
+		String part = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"; 
+		Pattern pt = Pattern.compile(part +"\\." +part+"\\."+part+"\\."+part);
+		while(!(line = reader.readLine()).equals("end") )
+		{
+			System.out.println(line);
+			Matcher matcher = pt.matcher(line);
+			if(matcher.matches())
+			{
+				res += "valid IP address\n";
+			}
+			else
+			{
+				res += "invalid IP address\n";
+			}
+				
+		}
+		res += "x\n";
+		return res;
+	}
+	
+	public String problem5(BufferedReader reader) throws IOException
+	{
+		String line;
+		String res="";
+		res+="5\n";
+		Pattern pt = Pattern.compile("[a-zA-Z_][a-zA-Z_\\d]*");
+		while(!(line = reader.readLine()).equals("end") )
+		{
+			System.out.println(line);
+			Matcher matcher = pt.matcher(line);
+			if(matcher.matches())
+			{
+				res += "valid c++ variable name\n";
+			}
+			else
+			{
+				res += "invalid c++ variable name\n";
+			}
+				
+		}
+		res += "x\n";
+		return res;
+	}
+	
+	public String problem6(BufferedReader reader) throws IOException
+	{
+		String line;
+		String res="";
+		res+="5\n";
+		Pattern pt = Pattern.compile("bbb", Pattern.CASE_INSENSITIVE);
+		while(!(line = reader.readLine()).equals("end") )
+		{
+			System.out.println(line);
+			Matcher matcher = pt.matcher(line);
+			if(matcher.find())
+			{
+				res += "invalid string, has 3 consecutive b's\n";
+			}
+			else
+			{
+				res += "valid string\n";
+			}
+				
+		}
+		res += "x\n";
+		return res;
+	}
+	
+	public String problem7(BufferedReader reader) throws IOException
+	{
+		
+	}
+	
+	public String problem8(BufferedReader reader) throws IOException
+	{
+		
+	}
+	
+	public String problem9(BufferedReader reader) throws IOException
+	{
+		
+	}
+	
+	public String problem10(BufferedReader reader) throws IOException
+	{
+		
+	}
+
+}
---
+++ Regex/src/main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) {
+        Problems problem = new Problems();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", true));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                switch (line) {
+                    case "1":
+                    	writer.append(problem.problem1(reader));
+                    	break;
+                    case "2":
+                        writer.append(problem.problem2(reader));
+                        break;
+                    case "3":
+                        writer.append(problem.problem3(reader));
+                        break;
+                    case "4":
+                        writer.append(problem.problem4(reader));
+                        break;
+                    case "5":
+                        writer.append(problem.problem5(reader));
+                        break;
+                    case "6":
+                        writer.append(problem.problem6(reader));
+                        break;
+//                    case "7":
+//                        writer.append(problem.problem7(reader));
+//                        break;
+//                    case "8":
+//                        writer.append(problem.problem8(reader));
+//                        break;
+//                    case "9":
+//                        writer.append(problem.problem9(reader));
+//                        break;
+//                    case "10":
+//                        writer.append(problem.problem10(reader));
+//                        break;
+                 }
+            }
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
